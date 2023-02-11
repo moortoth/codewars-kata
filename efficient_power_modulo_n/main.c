a20228e8d25df83001136b8620e337f1a9253d43
moortoth <iliabarynkin@yandex.ru>
2023-02-11 09:01:04 +0400

    added task descriptions and some minor tests

--- efficient_power_modulo_n/main.c
+++ efficient_power_modulo_n/main.c
@@ -1,6 +1,13 @@
 #include <inttypes.h>
 #include <assert.h>
 
+// https://www.codewars.com/kata/52fe629e48970ad2bd0007e6
+//
+// Your task is to create a new implementation of modpow so that it computes (x^y)%n for large y.
+// The problem with the current implementation is that the output of Math.pow is so large on our
+// inputs that it won't fit in a 64-bit float.
+// You're also going to need to be efficient, because we'll be testing some pretty big numbers.
+
 typedef uint64_t u64;
 
 u64 modpow(u64 x, u64 e, u64 n)
